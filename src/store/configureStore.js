3eb37f54026a541a85b6958ffdd87f7451c679e4
Salman <isalman19@hotmail.com>
2019-12-17 00:21:14 +0100

    feat: redux added to project & store is configured

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+export default function configureStore(preloadedState) {
+    const store = createStore(
+        reducers, 
+        preloadedState,
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
+}
