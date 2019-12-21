71d41339d809607b5f97f94fbc592906dcf9bfed
Salman <isalman19@hotmail.com>
2019-12-21 16:00:30 +0100

    feat: add pagination to offer list

--- src/containers/OfferList.js
+++ src/containers/OfferList.js
@@ -2,15 +2,54 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { OfferItem } from '../components';
 
-function OfferList ({ data }) {    
+function Numbers({ numberOfPages=0, goToPage, currentPage }){
+    let pages = [];
+    for(let i = 1; i <= numberOfPages; i++){
+        let active = i === currentPage ? 'active-page' : '';
+        pages.push( <button 
+            key={i} 
+            className={active} 
+            disabled = {i === currentPage}
+            onClick={() => goToPage(i)}>{i}</button> 
+        );
+    }
+    return pages;
+}
+
+function OfferList ({ data = [] }) {    
     const dispatch = useDispatch();
+    const [pageList, setPageList] = React.useState([]);
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const [numberPerPage] = React.useState(3);
+    const [numberOfPages, setNumberOfPages] = React.useState(0);
+    
+    const loadList = () => {
+        let begin = ((currentPage - 1) * numberPerPage);
+        let end = begin + numberPerPage;
+        let pageList = data.slice(begin, end);
+        setPageList(pageList);
+    }
+
+    React.useEffect(
+        () => {
+            loadList();
+            setNumberOfPages(Math.ceil(data.length / 3));
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [data, currentPage]
+    );
+    
+    const nextPage = () => setCurrentPage(currentPage + 1);
+    const prevPage = () => setCurrentPage(currentPage - 1);
+
     const apply = item => {
-        dispatch({type: 'APPLY_OFFER', item })
+        dispatch({type: 'APPLY_OFFER', item });
     };
+    
     return(
         <>
             {
-                data && data.map(item => {
+                pageList && pageList.map(item => {
                 return <OfferItem 
                         key = {item._id}
                         Item = {item}
@@ -18,6 +57,15 @@ function OfferList ({ data }) {
                         />
                 })
             }
+            <div className='pagination'>
+                <button onClick={prevPage} disabled={currentPage === 1}>{'<'}</button>
+                <Numbers 
+                    numberOfPages={numberOfPages} 
+                    goToPage={setCurrentPage} 
+                    currentPage={currentPage}
+                />
+                <button onClick={nextPage} disabled={currentPage === numberOfPages}>{'>'}</button>
+            </div>
         </>
     );
     
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export default function configureStore() {
-    const store = createStore(
-        reducers, 
-        compose(
-            applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+export default function configureStore(initialState) {
+    const store = createStore(reducers, initialState, compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
     return store;
 }
